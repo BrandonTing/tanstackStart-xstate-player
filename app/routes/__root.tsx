349c64df89b29e257189572daa16de1f7e50d421
Brandon Ting <brandonting@kkcompany.com>
2025-01-03 20:30:34 +0800

    Revert "enable react scan"
    This reverts commit cca41ec2e0d0c8c123a19a7d195f5d278ad43df7.

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -3,70 +3,59 @@ import { Header } from "@/components/Header";
 import "@/index.css";
 import css from "@/index.css?url";
 import {
-  Outlet,
-  ScrollRestoration,
-  createRootRoute,
-  useLocation,
-  useParams,
+	Outlet,
+	ScrollRestoration,
+	createRootRoute,
 } from "@tanstack/react-router";
 import { Meta, Scripts } from "@tanstack/start";
 import type { ReactNode } from "react";
-import { Monitoring } from "react-scan/monitoring";
+
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "TanStack Start Starter",
-      },
-    ],
-    links: [
-      {
-        href: css,
-        rel: "stylesheet",
-      },
-    ],
-  }),
-  component: RootComponent,
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "TanStack Start Starter",
+			},
+		],
+		links: [
+			{
+				href: css,
+				rel: "stylesheet",
+			},
+		],
+	}),
+	component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  );
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  const params = useParams({ strict: false })
-  const { pathname } = useLocation()
-  return (
-    <html lang="en-US">
-      <head>
-        <Meta />
-      </head>
-      <body className="text-white bg-black">
-        <div className="min-h-screen">
-          <Header />
-          {children}
-        </div>
-        <ScrollRestoration />
-        <Scripts />
-        <Monitoring
-          apiKey="uH8Itqx7cFX8-b4mXlj3JbzQ5qEpiFsa"
-          url="https://monitoring.react-scan.com/api/v1/ingest"
-          params={params}
-          path={pathname}
-        />
-
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en-US">
+			<head>
+				<Meta />
+			</head>
+			<body className="text-white bg-black">
+				<div className="min-h-screen">
+					<Header />
+					{children}
+				</div>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
