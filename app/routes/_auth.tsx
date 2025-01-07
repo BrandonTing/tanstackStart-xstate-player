b0277317ccf4937e5cc510a1ae9c069a4e76a6d3
Brandon Ting <brandonting@kkcompany.com>
2025-01-07 14:49:29 +0800

    handle auth protection

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,5 +1,6 @@
-import { useAuth } from '@clerk/tanstack-start';
-import { Navigate, Outlet, createFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
+import { SignInButton, SignUpButton, useAuth } from '@clerk/tanstack-start';
+import { Outlet, createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_auth')({
   component: RouteComponent,
@@ -11,7 +12,30 @@ function RouteComponent() {
     return <p className="pt-20 ">Loading...</p>;
   }
   if (!userId) {
-    return <Navigate to="/" />
+    return <div className='mt-20'>
+      <h1 className="mb-8 text-4xl font-bold text-center text-white">Sign In Required</h1>
+      <div className='flex items-center justify-center w-full'>
+        <div className="w-full max-w-md p-8 rounded-lg bg-zinc-800">
+          <p className="mb-6 text-lg text-center text-white">
+            Please sign in or sign up to access this content.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <Button
+              variant="ghost"
+              className="text-white hover:text-gray-900"
+            >
+              <SignInButton />
+            </Button>
+            <Button
+              variant="ghost"
+              className="text-white hover:text-gray-900"
+            >
+              <SignUpButton />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
   }
   return <Outlet />
 }
